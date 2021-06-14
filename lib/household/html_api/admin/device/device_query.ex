b3893c3bbb076069829160604b9f2d9513316d97
Hossein HassanNejad <hosseinspell@gmail.com>
2021-06-14 17:49:16 +0430

    query

--- lib/household/html_api/admin/device/device_query.ex
+++ lib/household/html_api/admin/device/device_query.ex
@@ -1,5 +1,5 @@
 defmodule Household.HtmlApi.Device.DeviceQuery do
-
+  import Ecto.Query
   alias Household.HtmlApi.Device.DeviceSchema
   alias Household.Repo
   def add_device(attrs) do
@@ -8,7 +8,14 @@ defmodule Household.HtmlApi.Device.DeviceQuery do
     |> Repo.insert()
   end
 
+  def devices do
+    query = from u in DeviceSchema,
+    order_by: [desc: u.inserted_at],
+    select: %{
+      name: u.name,
+      inserted_at: u.inserted_at,
+      updated_at: u.updated_at
+    }
+    Repo.all(query)
+  end
 end
-
-# <p class="alert alert-info" role="alert"><%= get_flash(@conn, :info) %></p>
-#       <p class="alert alert-danger" role="alert"><%= get_flash(@conn, :error) %></p>
--- lib/household_web/controllers/service_controller.ex
+++ lib/household_web/controllers/service_controller.ex
@@ -3,6 +3,10 @@ defmodule HouseholdWeb.ServiceController do
   alias Household.HtmlApi.Device.DeviceSchema
   alias Household.HtmlApi.Device.DeviceQuery
 
+  def devices(conn, _params) do
+    render(conn, "devices.html", devices: DeviceQuery.devices())
+  end
+
   def new_device(conn, _params) do
     changeset = DeviceSchema.changeset(%DeviceSchema{})
     render(conn, "new_device.html", changeset: changeset)
@@ -12,8 +16,11 @@ defmodule HouseholdWeb.ServiceController do
     case DeviceQuery.add_device(device) do
       {:ok, device_info} ->
         conn
+        |> put_flash(:success, "ذخیره شما با موفقیت انجام گردید.")
         |> redirect(to: "/new-device")
       {:error, changeset} ->
+        conn
+        |> put_flash(:danger, "مشکلی در فرم شما وجود دارد.")
         render(conn, "new_device.html", changeset: changeset)
     end
 
